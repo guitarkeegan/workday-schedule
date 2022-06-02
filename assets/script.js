aa8fa36b2975670c532da6815b1d92ea575c1e5b
guitarkeegan <keegananglim@gmail.com>
2022-06-02 13:02:11 -0700

    add TODOs

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
 var dt = dayjs()
 $("#currentDay").text(dt.format("dddd, MMM DD, YYYY"));
 console.log(dt);
+
+
+// TODO: create row for each hour/half-hour of the day
+// TODO: create 3 columns, for hour, task, save
+// TODO: color-code center divs for past, present, or future
+// TODO: add on click feature to edit center div
+// TODO: create button to save the text into the center div
+// TODO: save tasks in local storage
