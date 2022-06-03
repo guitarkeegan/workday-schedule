ab3716e19cd80f6eaba152a626d00030c7dfb164
guitarkeegan <keegananglim@gmail.com>
2022-06-02 20:34:54 -0700

    center hour text

--- assets/script.js
+++ assets/script.js
@@ -8,18 +8,20 @@ $("#currentDay").text(day.format("dddd, MMM DD, YYYY"));
 var jumbotronDiv = $(".jumbotron");
 var row = $("<div>").addClass("row");
 
-var hourDiv = $(`<div class='col-md-3 time-block hour'></div>`).text("9AM");
-var textArea = $("<textarea class='description' placeholder='add event here'>")
-var taskDiv = $("<div class='col-md-6 d-flex justify-items-start'></div>").append(textArea);
-taskDiv.addClass("present");
+
+var hourDiv = $(`<div class='col-md-2 time-block hour d-flex align-items-center justify-content-center'></div>`).text("9AM");
+
+var textArea = $("<textarea class='description col-md-8' placeholder='add event here'></textarea>")
+textArea.addClass("present");
+// taskDiv.addClass("present");
 // not working
 const saveIcon = $("<i class='fa-solid fa-floppy-disk'></i>");
-var saveDiv = $("<div class='col-md-3 saveBtn'></div>").html(saveIcon);
+var saveBtn = $("<button class='col-md-2 saveBtn'></button>").append(saveIcon);
 
 jumbotronDiv.append(row);
 row.append(hourDiv);
-row.append(taskDiv);
-row.append(saveDiv);
+row.append(textArea);
+row.append(saveBtn);
 
 
 
